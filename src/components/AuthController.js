0e006f1aee11b496782b8908bdaeb3d2fd82cee9
Павел Хрипунов <khripunovpp@gmail.com>
2020-02-13 19:04:22 +0300

    added logout when has access_error into AuthController

--- src/components/AuthController.js
+++ src/components/AuthController.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useState, useEffect } from "react";
 import setToken from "../store/actions/setToken.js";
 import { connect } from "react-redux";
+import logout from '../store/actions/logout.js';
 
-function WelcomePage({ location, setToken }) {
+function WelcomePage({ location, setToken, logout }) {
   const [accessError, setAccessError] = useState(false);
 
   const queryString = location.hash.slice(1);
@@ -10,6 +11,11 @@ function WelcomePage({ location, setToken }) {
   const setTokenToStore = (q) => {
     setToken(q).then(() => (window.location = "./"));
   };
+  
+
+  useEffect(() => {
+    accessError && logout();
+  }, [accessError]);
   
   useEffect(() => {
     queryString.includes("token")
@@ -21,6 +27,7 @@ function WelcomePage({ location, setToken }) {
 }
 
 const mapDispatchToProps = {
-  setToken
+  setToken,
+  logout
 };
 export default connect(null, mapDispatchToProps)(WelcomePage);
