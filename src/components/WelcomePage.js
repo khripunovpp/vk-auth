a6a9e8cc161e0819a95e7333cbbcdb108810f296
Павел Хрипунов <khripunovpp@gmail.com>
2020-02-17 14:30:43 +0300

    rewrite WelcomePage via useDispatch hook

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,25 +1,24 @@
 import React, { Fragment } from "react";
 import login from "../store/actions/login.js";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import Panel from "./Panel.js";
 import Button from "./Button.js";
 import Header from "./Header.js";
 import MainSection from "./MainSection.js";
 
-const WelcomePage = ({ login }) => (
-  <Fragment>
-    <Header />
-    <MainSection>
-      <Panel>
-        <h1 className="panel__title">Hi there, signin to continue</h1>
-        <Button onClick={login} sign="Login" />
-      </Panel>
-    </MainSection>
-  </Fragment>
-);
-
-const mapDispatchToProps = {
-  login
+const WelcomePage = () => {
+  const dispatch = useDispatch();
+  return (
+    <Fragment>
+      <Header />
+      <MainSection>
+        <Panel>
+          <h1 className="panel__title">Hi there, signin to continue</h1>
+          <Button onClick={() => dispatch(login())} sign="Login" />
+        </Panel>
+      </MainSection>
+    </Fragment>
+  );
 };
 
-export default connect(null, mapDispatchToProps)(WelcomePage);
+export default WelcomePage;
