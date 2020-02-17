345d16af9ac5ac044bf78ba62177db473309b196
Павел Хрипунов <khripunovpp@gmail.com>
2020-02-17 15:59:31 +0300

    rewrite Layout via useDispatch hook

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import logout from '../store/actions/logout.js';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-const Logout = ({logout}) => {
-    return(
-        <button className="btn btn--link logout" onClick={logout}>Logout</button>
+const Logout = () => {
+    const dispatch = useDispatch();
+
+    return (
+        <button className="btn btn--link logout"onClick={() => dispatch(logout())}>Logout</button>
     )
 }
 
-const mapDispatchToProps = {
-    logout
-}
-
-export default connect(null, mapDispatchToProps)(Logout)
+export default Logout
